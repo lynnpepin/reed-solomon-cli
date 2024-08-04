786dc95cea7f4caebc0db07858795646bc642304
lynn pepin <lynnpepin@tutanota.com>
2024-08-04 12:53:07 -0400

    feat: CLI parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+
+    /// If verbose is set, print extra messages while running.
+    #[arg(short, long)]
+    verbose: Option<bool>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Reed-Solomon encode an input file, to a given output file
+    Encode {
+        /// Input filename
+        #[arg(short, long)]
+        input: String,
+
+        /// Output filename (will be overwritten)
+        #[arg(short, long)]
+        output: String,
+    },
+
+    /// Decode a Reed-Solomon encoded file to restore the original input.
+    Decode {
+        /// Input filename
+        #[arg(short, long)]
+        input: String,
+
+        /// Output filename (will be overwritten)
+        #[arg(short, long)]
+        output: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{args:?}");
+
+    match &args.command {
+        Commands::Encode {input, output} => {
+            println!("todo; encode {input} to {output}");
+        },
+        Commands::Decode {input, output} => {
+            println!("todo; encode {input} to {output}");
+        }
+    }
 }
